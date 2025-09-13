5113f378765c9c48f2745943eee79b07b535f439
tangfire <52628216@qq.com>
2025-09-13 20:28:28 +0800

    准备4.62

--- internal/data/review.go
+++ internal/data/review.go
@@ -2,7 +2,9 @@ package data
 
 import (
 	"context"
+	"errors"
 	"review-service/internal/data/model"
+	"review-service/internal/data/query"
 
 	"review-service/internal/biz"
 
@@ -37,12 +39,43 @@ func (r *reviewRepo) GetReviewByOrderId(ctx context.Context, orderId int64) ([]*
 		Find()
 }
 
-func (r *reviewRepo) SaveReply(ctx context.Context, info *model.ReviewReplyInfo) (*model.ReviewReplyInfo, error) {
+func (r *reviewRepo) SaveReply(ctx context.Context, reply *model.ReviewReplyInfo) (*model.ReviewReplyInfo, error) {
 	// 1. 数据校验
 	// 1.1 数据合法性校验(已回复的评价不允许商家再次回复)
+	// 先用评价ID查库，看下是否已回复
+	review, err := r.data.query.ReviewInfo.WithContext(ctx).Where(r.data.query.ReviewInfo.ReviewID.Eq(reply.ReplyID)).First()
+	if err != nil {
+		return nil, err
+	}
+	if review.HasReply == 1 {
+		return nil, errors.New("该评价已回复")
+	}
+
 	// 1.2 水平越权校验(A商家只能回复自己的不能回复B商家的)
+	// 举例子: 用户A删除订单，userId + orderId 当条件去查询订单然后删除
+	if review.StoreID != reply.StoreID {
+		return nil, errors.New("水平越权")
+	}
+
 	// 2. 更新数据库中的数据(评价回复表和评价表要同时更新，涉及到事务操作)
+	// 事务操作
+	err = r.data.query.Transaction(func(tx *query.Query) error {
+		// 回复表插入一条数据
+		if err := tx.ReviewReplyInfo.
+			WithContext(ctx).
+			Save(reply); err != nil {
+			r.log.WithContext(ctx).Errorf("SaveReply create reply fail,err:%v", err)
+			return err
+		}
+		// 评价表更新hasReply字段
+		if _, err := tx.ReviewInfo.WithContext(ctx).Where(tx.ReviewInfo.ReviewID.Eq(reply.ReplyID)).Update(tx.ReviewInfo.HasReply, 1); err != nil {
+			r.log.WithContext(ctx).Errorf("SaveReply update reply fail,err:%v", err)
+			return err
+		}
+		return nil
+	})
+
 	// 3. 返回
-	return nil, nil
+	return reply, err
 
 }
